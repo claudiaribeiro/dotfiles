b880c439dccff3e3c2a797c0107b0cf895e01099
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2018-07-06 01:32:21 +0100

    Update dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,10 +2,9 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/iTerm2.app"
 
 killall Dock
